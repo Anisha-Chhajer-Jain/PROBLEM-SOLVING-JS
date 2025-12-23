b1ac4f226be60bf6664e0360b8780f8984f82cc5
Anisha Chhajer <anishachhajer16@gmail.com>
2025-12-23 14:53:51 +0530

    done

---
+++ Level-14/175.js
@@ -0,0 +1,12 @@
+
+// Q175. Elements at Even Indices
+function evenIndexElements(arr) {
+    let res = [];
+
+    for (let i = 0; i < arr.length; i += 2) {
+        res.push(arr[i]);
+    }
+
+    return res;
+}
+
