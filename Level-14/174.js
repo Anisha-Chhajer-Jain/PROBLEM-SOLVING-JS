27e9bf897b8cb612c46350e61058c8ca62ed155b
Anisha Chhajer <anishachhajer16@gmail.com>
2025-12-23 14:53:18 +0530

    uploaded 174 question

---
+++ Level-14/174.js
@@ -0,0 +1,12 @@
+
+// Q174. Maximum Difference (max - min)
+function maxDifference(arr) {
+    let min = arr[0], max = arr[0];
+
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] < min) min = arr[i];
+        if (arr[i] > max) max = arr[i];
+    }
+
+    return max - min;
+}
