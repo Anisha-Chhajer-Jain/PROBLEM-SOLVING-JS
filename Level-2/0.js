9f3bdc048a67c1cf9a86e7c7863c1c41f91e6b54
Anisha Chhajer <anishachhajer16@gmail.com>
2025-12-24 11:41:33 +0530

    15

--- Level-2/0.js
+++ Level-2/0.js
@@ -1,16 +1,5 @@
 
 
-// Q15. Sum 1 to N
-// Method 1: loop sum
-let N = 5, sum = 0;
-for (let i = 1; i <= N; i++) {
-  sum += i;
-}
-console.log(sum);
-
-// Method 2: formula
-
-console.log((N * (N + 1)) / 2);
 
 // Q16. Product 1 to N
 // Method 1: loop
