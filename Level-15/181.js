876369c3797df232c9aa2c353577fcab6818869f
Anisha Chhajer <anishachhajer16@gmail.com>
2025-12-24 12:10:22 +0530

    done with today

--- Level-15/181.js
+++ Level-15/181.js
@@ -1,3 +1,28 @@
 // Q181. Sum of Odd Numbers in Array...Return sum of only odd numbers.
+function oddSum(arr){
+    let sum =0;
+    for(let i=0;i<arr.length;i++){
+        if(arr[i]%2==1){
+            sum = sum + arr[i];
+        }
+    }
+    console.log(sum);
+}
+oddSum([1,2,3,4,5,6]);
 
+//--------------------------------------------------------------------
 
+function sumOdd1(data){
+    let sum = 0;
+    let res=data.reduce((sum,num)=>{
+        if(num%2!=0){
+            return sum+num;
+        }
+        else{
+            return sum;
+        }
+    },0)
+    console.log(res);
+}
+
+sumOdd1([73,36,22,43,98,40]);
