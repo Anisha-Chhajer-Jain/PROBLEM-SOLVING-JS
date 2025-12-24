da95a8a4c8c041a49d6e567b7ae193fd64dd6984
Anisha Chhajer <anishachhajer16@gmail.com>
2025-12-24 11:42:23 +0530

    16 and 17

---
+++ Level-2/16.js
@@ -0,0 +1,15 @@
+// Q16. Product 1 to N
+// Method 1: loop
+let N = 4, product = 1;
+for (let i = 1; i <= N; i++) {
+  product *= i;
+}
+console.log(product);
+
+// Method 2: while loop
+// let N = 5, i = 1, product = 1;
+while (i <= N) {
+  product *= i;
+  i++;
+}
+console.log(product);
---
+++ Level-2/17.js
@@ -0,0 +1,13 @@
+
+// Q17. Multiplication Table of a Number
+// Method 1: normal loop
+let n = 5;
+for (let i = 1; i <= 10; i++) {
+  console.log(n * i);
+}
+
+// Method 2: while loop
+while (i <= 10) {
+  console.log(n * i);
+  i++;
+}
