952a719d25314a80cb1ecb98d1666e00747dbcae
Anisha Chhajer <anishachhajer16@gmail.com>
2025-12-27 11:50:15 +0530

    selection sort

--- Level-15/187.js
+++ Level-15/187.js
@@ -3,16 +3,15 @@
 function selectionSort(arr) {
     let n = arr.length;
 
-    for (let i = 0; i < n; i++) {
-        let minIdx = i;
-        for (let j = i + 1; j < n; j++) {
-            if (arr[j] < arr[minIdx]) {
-                minIdx = j;
+    for (let i=0;i<n;i++) {
+        let min = i;
+        for (let j=i+1;j<n;j++) {
+            if (arr[j]<arr[min]) {
+                min=j;
             }
         }
-        [arr[i], arr[minIdx]] = [arr[minIdx], arr[i]];
+        [arr[i],arr[min]] = [arr[min],arr[i]];
     }
     return arr;
 }
-
 console.log(selectionSort([3,1,4,2]));
