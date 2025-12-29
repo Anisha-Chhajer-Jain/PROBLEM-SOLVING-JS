86722280f0f41ab0994976d55751619d2157042a
Anisha Chhajer <anishachhajer16@gmail.com>
2025-12-29 11:58:19 +0530

    insertion sort

--- Level-15/188.js
+++ Level-15/188.js
@@ -14,3 +14,19 @@ function insertionSort(arr) {
     return arr;
 }
 console.log(insertionSort([5,4,3,2,1]));
+
+//---------------------------- BY SIR ----------------------------------
+
+function insertionSort(arr) {
+    for (let i = 1; i < arr.length; i++) { 
+        for (let j = i; j > 0; j--) {
+            if (arr[j] < arr[j - 1]) {
+                [arr[j], arr[j - 1]] = [arr[j - 1], arr[j]];
+            } else {
+                break; 
+            }
+        }
+    }
+    console.log(arr);
+}
+insertionSort([5, 2, 9, 1, 5, 6]); 
