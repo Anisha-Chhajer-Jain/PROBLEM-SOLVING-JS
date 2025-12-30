f483e676f198a67fa993f0d34285cc0b2196d816
Anisha Chhajer <anishachhajer16@gmail.com>
2025-12-30 14:10:49 +0530

    upto 190

--- Level-15/189.js
+++ Level-15/189.js
@@ -7,3 +7,19 @@ function kthSmallest(arr, k) {
 }
 
 console.log(kthSmallest([7,10,4,3,20,15], 3)); 
+
+//----------bubble sort-----------------
+function kthSmallest(arr, k) {
+    let a = [...arr]; 
+    for (let i = 0; i < a.length - 1; i++) {
+        for (let j = 0; j < a.length - i - 1; j++) {
+            if (a[j] > a[j + 1]) {
+                let temp = a[j];
+                a[j] = a[j + 1];
+                a[j + 1] = temp;
+            }
+        }
+    }
+    return a[k - 1];
+}
+console.log(kthSmallest([7, 10, 4, 3, 20, 15], 3));
--- Level-15/190.js
+++ Level-15/190.js
@@ -1,21 +1,43 @@
 // Q190. k-th Largest Element (Using Sort) — 3 Ways
-// Way 1: Sort Descending
+
 function kthLargest1(arr, k) {
     let sorted = [...arr].sort((a, b) => b - a);
     return sorted[k - 1];
 }
-// Way 2: Sort Ascending
+//-------------------------------------------------------------------------------
+function kthLargest(arr, k) {
+    let a = [...arr]; 
+    for (let i = 1; i < a.length; i++) {
+        let key = a[i];
+        let j = i - 1;
 
-function kthLargest2(arr, k) {
-    let sorted = [...arr].sort((a, b) => a - b);
-    return sorted[sorted.length - k];
-}
-//Way 3: Using Max Removal
-function kthLargest3(arr, k) {
-    let temp = [...arr];
-    for (let i = 0; i < k - 1; i++) {
-        temp.splice(temp.indexOf(Math.max(...temp)), 1);
+        while (j >= 0 && a[j] < key) {
+            a[j + 1] = a[j];
+            j--;
+        }
+        a[j + 1] = key;
     }
-    return Math.max(...temp);
+
+    return a[k - 1];
 }
-console.log(kthLargest1([7,10,4,3,20,15], 2)); 
+
+console.log(kthLargest([7, 10, 4, 3, 20, 15], 3));
+
+
+
+
+
+
+// function kthLargest2(arr, k) {
+//     let sorted = [...arr].sort((a, b) => a - b);
+//     return sorted[sorted.length - k];
+// }
+
+// function kthLargest3(arr, k) {
+//     let temp = [...arr];
+//     for (let i = 0; i < k - 1; i++) {
+//         temp.splice(temp.indexOf(Math.max(...temp)), 1);
+//     }
+//     return Math.max(...temp);
+// }
+// console.log(kthLargest1([7,10,4,3,20,15], 2)); 
